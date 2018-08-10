fbb9c267eb94b4a0c12636df4f61531506a4c44b
Maxim <maxjust@inbox.ru>
2018-08-10 17:56:02 +0300

    Добавьте файлы проекта.

---
+++ ToDoListApp/ToDoListApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoListApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
